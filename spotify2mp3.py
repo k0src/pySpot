29dfd5d7b2e2bd498dd12c8342e2f103495716ee
k0 <k0@k0src.com>
2024-03-29 17:04:51 -0500

    added format buttons

--- spotify2mp3.py
+++ spotify2mp3.py
@@ -7,6 +7,8 @@ from tkinter import filedialog
 import os
 import keys
 
+# id3 tag editing - option
+
 client_id = keys.client_id
 client_secret = keys.client_secret
 
@@ -16,6 +18,8 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 def download_mp3(spotlink):
 
     if 'playlist' in spotlink:
+        frmt = ''
+
         songs_num = 0
 
         directory_path = filedialog.askdirectory()
@@ -24,6 +28,15 @@ def download_mp3(spotlink):
 
         results = sp.playlist_tracks(playlist_id)
         tracks = results['items']
+
+        if format_var.get() == 0:
+            frmt = ".mp3"
+        elif format_var.get() == 1:
+            frmt = ".wav"
+        elif format_var.get() == 2:
+            frmt = ".flac"
+        elif format_var.get() == 3:
+            frmt = ".ogg"
         
         for track in tracks:
             track_id = track['track']['id']
@@ -42,7 +55,7 @@ def download_mp3(spotlink):
             video = yt.streams.filter(only_audio=True).first()
             out_file = video.download(output_path=directory_path)
             base, ext = os.path.splitext(out_file)
-            new_file = base + '.mp3'
+            new_file = base + frmt
             os.rename(out_file, new_file)
 
             songs_num += 1
@@ -51,6 +64,8 @@ def download_mp3(spotlink):
         msg_var.set(f"{songs_num} songs successfully downloaded!")
         
     elif 'track' in spotlink:
+        frmt = ''
+
         directory_path = filedialog.askdirectory()
 
         track_id = spotlink.split('/')[-1].split('?')[0]
@@ -66,10 +81,19 @@ def download_mp3(spotlink):
 
         yt = YouTube(ytlink)
 
+        if format_var.get() == 0:
+            frmt = ".mp3"
+        elif format_var.get() == 1:
+            frmt = ".wav"
+        elif format_var.get() == 2:
+            frmt = ".flac"
+        elif format_var.get() == 3:
+            frmt = ".ogg"
+
         video = yt.streams.filter(only_audio=True).first()
         out_file = video.download(output_path=directory_path)
         base, ext = os.path.splitext(out_file)
-        new_file = base + '.mp3'
+        new_file = base + frmt
         os.rename(out_file, new_file)
 
         msg.config(foreground='lightblue')
@@ -81,12 +105,13 @@ def download_mp3(spotlink):
 
 window = ttk.Window(themename="darkly")
 window.title("pySpot")
-window.geometry('500x200')
-window.maxsize(500, 200)
-window.minsize(500, 200)
+window.geometry('500x250')
+window.maxsize(500, 250)
+window.minsize(500, 250)
 
 link_var = tk.StringVar()
 msg_var = tk.StringVar()
+format_var = tk.IntVar()
 
 title = ttk.Label(window, text="pySpot", font = 'Calibri 24 bold', foreground='lightblue')
 title.pack(pady=5)
@@ -99,6 +124,19 @@ link_entry.pack(pady=5)
 
 download_button = ttk.Button(window, text="Download", command=lambda: download_mp3(link_var.get()))
 download_button.pack(pady=5)
+
+radio_frame = ttk.Frame(window)
+
+f1 = ttk.Radiobutton(radio_frame, text="mp3", variable=format_var, value=0)
+f1.pack(side='left', padx=5, pady=5)
+f2 = ttk.Radiobutton(radio_frame, text="wav", variable=format_var, value=1)
+f2.pack(side='left', padx=5, pady=5)
+f3 = ttk.Radiobutton(radio_frame, text="flac", variable=format_var, value=2)
+f3.pack(side='left', padx=5, pady=5)
+f4 = ttk.Radiobutton(radio_frame, text="ogg", variable=format_var, value=3)
+f4.pack(side='left', padx=5, pady=5)
+
+radio_frame.pack(pady=5)
 
 msg = ttk.Label(window, textvariable=msg_var, font = 'Calibri 10')
 msg.pack(pady=5)
